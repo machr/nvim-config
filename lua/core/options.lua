1fae0358a99fe46dca6c9d6e18c6af88e1e464f2
Mark Christiansen <hi@powellmark.com>
2024-04-28 21:40:27 +1000

    set up nvim-cmp

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -2,32 +2,32 @@
 local M = {}
 
 M.config = function()
-    vim.opt.showmode = false
-    vim.opt.cursorline = true
-    vim.opt.clipboard = 'unnamed'
-    vim.opt.ignorecase = true
-    vim.opt.smartcase = true
-    vim.opt.infercase = true
-    vim.opt.number = true
-    vim.opt.relativenumber = true
-    vim.opt.tabstop = 2
-    vim.opt.shiftwidth = 2
-    vim.opt.expandtab = true
-    vim.opt.foldenable = false
-    vim.opt.foldmethod = 'expr'
-    vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
-    vim.opt.timeoutlen = 400
-    vim.opt.ttimeoutlen = 10
-    vim.opt.termguicolors = true
-    vim.opt.mouse = 'nvi'
-    vim.opt.signcolumn = 'yes'
-    vim.opt.updatetime = 100
-    vim.opt.conceallevel = 0
-    vim.g.do_filetype_lua = 1
-    -- vim.opt.cmdheight = 0
+	vim.opt.showmode = false
+	vim.opt.cursorline = true
+	vim.opt.clipboard = "unnamed"
+	vim.opt.ignorecase = true
+	vim.opt.smartcase = true
+	vim.opt.infercase = true
+	vim.opt.number = true
+	vim.opt.relativenumber = true
+	vim.opt.tabstop = 2
+	vim.opt.shiftwidth = 2
+	vim.opt.expandtab = true
+	vim.opt.foldenable = false
+	vim.opt.foldmethod = "expr"
+	vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+	vim.opt.timeoutlen = 400
+	vim.opt.ttimeoutlen = 10
+	vim.opt.termguicolors = true
+	vim.opt.mouse = "nvi"
+	vim.opt.signcolumn = "yes"
+	vim.opt.updatetime = 100
+	vim.opt.conceallevel = 0
+	vim.g.do_filetype_lua = 1
+	-- vim.opt.cmdheight = 0
 
-    -- color scheme config
-    vim.opt.background = 'dark'
+	-- color scheme config
+	vim.opt.background = ""
 end
 
 return M
--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -5,52 +5,54 @@ return {
         "nvim-lua/plenary.nvim",
         "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
         "MunifTanjim/nui.nvim",
-      's1n7ax/nvim-window-picker',
+        's1n7ax/nvim-window-picker',
     },
-  config = function()
-     require('window-picker').setup()
-    require('neo-tree').setup({
+    config = function()
+        require('window-picker').setup()
+        require('neo-tree').setup({
 
-default_component_configs = {
-          indent = {
-            with_expanders = true,
-          },
-        },
-        window = {
-          position = "float",
-          mapping_options = {
-            noremap = true,
-            nowait = true,
-          },
-          mappings = {
-            ["o"] = "open_with_window_picker",
-            ["x"] = "close_node",
-            ["X"] = "cut_to_clipboard",
-            ["W"] = "close_all_nodes",
-            ["z"] = "",
-            ["H"] = "",
-            ["h"] = "toggle_hidden",
-            ["/"] = "",
-          },
-        },
-        filesystem = {
-          follow_current_file = true,
-          filtered_items = {
-            hide_dotfiles = false,
-            hide_gitignored = false,
-          },
-        },
-        event_handlers = {
-          {
-            event = "neo_tree_buffer_enter",
-            handler = function()
-              vim.cmd [[ setlocal relativenumber ]]
-            end
-          },
-        },
-      })
-      local set = vim.keymap.set
-      local opts = { noremap = true, silent = true }
-     set('n', '<leader>b', 'Neotree toggle reveal reveal_force_cwd<cr>', opts)
-  end
+            default_component_configs = {
+                indent = {
+                    with_expanders = true,
+                },
+            },
+            window = {
+                position = "float",
+                mapping_options = {
+                    noremap = true,
+                    nowait = true,
+                },
+                mappings = {
+                    ["o"] = "open_with_window_picker",
+                    ["x"] = "close_node",
+                    ["X"] = "cut_to_clipboard",
+                    ["W"] = "close_all_nodes",
+                    ["z"] = "",
+                    ["H"] = "",
+                    ["h"] = "toggle_hidden",
+                    ["/"] = "",
+                },
+            },
+            filesystem = {
+                follow_current_file = {
+                    enable = true,
+                },
+                filtered_items = {
+                    hide_dotfiles = false,
+                    hide_gitignored = false,
+                },
+            },
+            event_handlers = {
+                {
+                    event = "neo_tree_buffer_enter",
+                    handler = function()
+                        vim.cmd [[ setlocal relativenumber ]]
+                    end
+                },
+            },
+        })
+        local set = vim.keymap.set
+        local opts = { noremap = true, silent = true }
+        set('n', '<leader>b', 'Neotree toggle reveal reveal_force_cwd<cr>', opts)
+    end
 }
