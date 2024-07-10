72dfeb32d5bb79143894ee88de0ec6ffe834d2f2
Mark Christiansen <machr@users.noreply.github.com>
2024-07-10 13:53:31 +1000

    add conform

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,15 @@
+return {
+  "stevearc/conform.nvim",
+  formatters_by_ft = {
+    lua = { "stylua" },
+    -- Conform will run multiple formatters sequentially
+    go = { "goimports", "gofmt" },
+    -- Use a sub-list to run only the first available formatter
+    javascript = { { "prettierd", "prettier" } },
+  },
+  format_on_save = {
+    -- I recommend these options. See :help conform.format for details.
+    lsp_format = "fallback",
+    timeout_ms = 500,
+  },
+}
