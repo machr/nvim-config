d16345e6f16265049667709e86caf7aa8f355557
Mark Christiansen <machr@users.noreply.github.com>
2024-07-15 13:23:44 +1000

    change colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,48 +1,45 @@
 return {
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		enabled = true,
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.cmd("colorscheme catppuccin-mocha")
-		end,
-	},
-	{
-		"rebelot/kanagawa.nvim",
-		enabled = false,
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.cmd("colorscheme kanagawa")
-		end,
-	},
-	{
-		"maxmx03/fluoromachine.nvim",
-		enabled = false,
-		lazy = false,
-		priority = 10001,
-		config = function()
-			local fm = require("fluoromachine")
+  "folke/tokyonight.nvim",
+  priority = 1000,
+  config = function()
+    local transparent = false -- set to true if you would like to enable transparency
 
-			fm.setup({
-				glow = false,
-				theme = "fluoromachine",
-			})
+    local bg = "#011628"
+    local bg_dark = "#011423"
+    local bg_highlight = "#143652"
+    local bg_search = "#0A64AC"
+    local bg_visual = "#275378"
+    local fg = "#CBE0F0"
+    local fg_dark = "#B4D0E9"
+    local fg_gutter = "#627E97"
+    local border = "#547998"
 
-			vim.cmd.colorscheme("fluoromachine")
-		end,
-	},
+    require("tokyonight").setup({
+      style = "moon",
+      transparent = transparent,
+      styles = {
+        sidebars = transparent and "transparent" or "dark",
+        floats = transparent and "transparent" or "dark",
+      },
+      on_colors = function(colors)
+        colors.bg = bg
+        colors.bg_dark = transparent and colors.none or bg_dark
+        colors.bg_float = transparent and colors.none or bg_dark
+        colors.bg_highlight = bg_highlight
+        colors.bg_popup = bg_dark
+        colors.bg_search = bg_search
+        colors.bg_sidebar = transparent and colors.none or bg_dark
+        colors.bg_statusline = transparent and colors.none or bg_dark
+        colors.bg_visual = bg_visual
+        colors.border = border
+        colors.fg = fg
+        colors.fg_dark = fg_dark
+        colors.fg_float = fg
+        colors.fg_gutter = fg_gutter
+        colors.fg_sidebar = fg_dark
+      end,
+    })
 
-	{
-		"folke/tokyonight.nvim",
-		enabled = false,
-		config = function()
-			vim.cmd("colorscheme tokyonight-moon")
-			vim.cmd("highlight CursorLineNr guifg=orange")
-			vim.cmd("highlight CursorLine guibg=#3f3d3b")
-			vim.cmd("highlight LineNr guifg=#5b5b5b")
-		end,
-	},
+    vim.cmd("colorscheme tokyonight")
+  end,
 }
